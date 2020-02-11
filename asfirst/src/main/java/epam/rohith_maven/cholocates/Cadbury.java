afd66cc21eeb335bc9546105298c34a2f049a273
Nikhil Satya <nikhilssn10@gmail.com>
2020-02-11 10:27:39 +0530

    initial commit

---
+++ asfirst/src/main/java/epam/rohith_maven/cholocates/Cadbury.java
@@ -0,0 +1,9 @@
+package epam.rohith_maven.cholocates;
+
+public class Cadbury extends Chocolate {
+
+    public Cadbury(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
+
